0704d318f81dba9a25e9f1ab944cc0351b8911a3
abdellah <abdellah.marwan@gmail.com>
2019-11-04 20:27:07 +0100

    Minor.

--- src/main.cu
+++ src/main.cu
@@ -1,42 +1,47 @@
 #include "RandomWalk.h"
-#define N 1000 // Number of photons 
 
+#define N 1000 // Number of photons
 
+// Forward declarations
 void streamOut(float* _cpuX, float* _cpuY , float* _cpuZ);
 
-__global__ void finalPosition(unsigned int seed, curandState_t* states, float* _gpuX, float* _gpuY, float* _gpuZ) {
+__global__
+void finalPosition(unsigned int seed, curandState_t* states,
+                   float* _gpuX, float* _gpuY, float* _gpuZ)
+{
     curand_init(seed, blockIdx.x, 0, &states[blockIdx.x]);
 
-    Point finalPos; //should pass states here as an arg
+    Point finalPos;
     finalPos = randomWalk(states);
+
     _gpuX[blockIdx.x] = finalPos.getX();
     _gpuY[blockIdx.x] = finalPos.getY();
     _gpuZ[blockIdx.x] = finalPos.getZ();
 
 }
 
-
-  int main() {
+int main()
+{
     curandState_t* states;
     cudaMalloc((void**) &states, N * sizeof(curandState_t));
 
-// Allocate host memory for final positions
+    // Allocate host memory for final positions
     float * _cpuX= (float*)malloc(sizeof(float) * N);
     float * _cpuY= (float*)malloc(sizeof(float) * N);
     float * _cpuZ= (float*)malloc(sizeof(float) * N);
 
-// Allocate device  memory for final positions
+    // Allocate device  memory for final positions
     float* _gpuX = nullptr;
     cudaMalloc((void**) &_gpuX, N * sizeof(float));
     float* _gpuY = nullptr;
     cudaMalloc((void**) &_gpuY, N * sizeof(float));
     float* _gpuZ = nullptr;
     cudaMalloc((void**) &_gpuZ, N * sizeof(float));
-  
-// Call Kernel
+
+    // Call Kernel
     finalPosition<<<N , 1>>>(time(0), states , _gpuX, _gpuY, _gpuZ);
 
-// Copy device data to host memory to stream them out
+    // Copy device data to host memory to stream them out
     cudaMemcpy(_cpuX, _gpuX, N * sizeof( float), cudaMemcpyDeviceToHost);
     cudaMemcpy(_cpuY, _gpuY, N * sizeof( float), cudaMemcpyDeviceToHost);
     cudaMemcpy(_cpuZ, _gpuZ, N * sizeof( float), cudaMemcpyDeviceToHost);
@@ -44,10 +49,11 @@ __global__ void finalPosition(unsigned int seed, curandState_t* states, float* _
     // Stream out final position of each photon to file
     streamOut (&_cpuX[0], &_cpuY[0], &_cpuZ[0]);
 
-// Free Memory
+    // Free Memory
     free(_cpuX);
     free(_cpuY);
     free(_cpuZ);
+
     cudaFree(_gpuX);
     cudaFree(_gpuY);
     cudaFree(_gpuZ);
