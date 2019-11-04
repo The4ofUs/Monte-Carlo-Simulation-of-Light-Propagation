000f34535d87faaa48112913d7036eafb5e5624a
abdellah <abdellah.marwan@gmail.com>
2019-11-04 20:15:48 +0100

    Cleaning the Ray class.

--- src/Ray.cu
+++ src/Ray.cu
@@ -3,25 +3,51 @@ using namespace std;
 #include <iostream>
 
 __device__ 
-void Ray::startFrom(Point startingPoint) // Sets getYour starting point
+void Ray::startFrom(Point startingPoint)
 {
-    this->_currentPos.setCoordinates(startingPoint.getX(), startingPoint.getY(), startingPoint.getZ());
+    _currentPos.setCoordinates(startingPoint.getX(),
+                                     startingPoint.getY(),
+                                     startingPoint.getZ());
 }
 
-__device__ void Ray::setDirection(Point direction) { this->_direction.setCoordinates(direction.getX(), direction.getY(), direction.getZ()); }
-
-__device__ void Ray::setStep(float step) { this->_step = step; }
-
-__device__ Point Ray::getCurrentPos() const { return this->_currentPos; }
-
-__device__ Point Ray::getDirection() const { return this->_direction; }
-
-__device__ float Ray::getStep() const { return this->_step; }
-
-__device__ void Ray::move() // The point moves in the specified direction with the given step
+__device__
+void Ray::setDirection(Point direction)
 {
-    float newX = this->_currentPos.getX() + (this->_direction.getX() * this->getStep());
-    float newY = this->_currentPos.getY() + (this->_direction.getY() * this->getStep());
-    float newZ = this->_currentPos.getZ() + (this->_direction.getZ() * this->getStep());
-    this->_currentPos.setCoordinates(newX, newY, newZ);
+    _direction.setCoordinates(direction.getX(),
+                                    direction.getY(),
+                                    direction.getZ());
+}
+
+__device__
+void Ray::setStep(float step)
+{
+    _step = step;
+}
+
+__device__
+Point Ray::getCurrentPos() const
+{
+    return _currentPos;
+}
+
+__device__
+Point Ray::getDirection() const
+{
+    return _direction;
+}
+
+__device__
+float Ray::getStep() const
+{
+    return _step;
+}
+
+__device__
+void Ray::move()
+{
+    const float newX = _currentPos.getX() + (_direction.getX() * _step);
+    const float newY = _currentPos.getY() + (_direction.getY() * _step);
+    const float newZ = _currentPos.getZ() + (_direction.getZ() * _step);
+
+    _currentPos.setCoordinates(newX, newY, newZ);
 }
--- src/Ray.h
+++ src/Ray.h
@@ -1,22 +1,92 @@
 #ifndef Ray_H
 #define Ray_H
+
 #include "RandomnessGenerator.h"
 
+/**
+ * @brief The Ray class
+ */
 class Ray
 {
-
 public:
-__device__ void startFrom(Point startingPoint); // Sets the ray starting point
-__device__ void setDirection(Point direction);  // Sets the direction of the ray 
-__device__ void setStep(float step);           // Sets the step of movement
-__device__ Point getCurrentPos() const;
-__device__ Point getDirection() const;
-__device__ float getStep() const;
-__device__ void move(); // The point moves in the specified direction with the given step -The function relies on member attributes that getYou should set first-
+
+    /**
+     * @brief startFrom
+     * Sets the ray starting point.
+     * @param startingPoint
+     * The starting point of the ray.
+     */
+    __device__
+    void startFrom(Point startingPoint);
+
+    /**
+     * @brief setDirection
+     * Sets the direction of the ray
+     * @param direction
+     * The direction of the ray.
+     */
+    __device__
+    void setDirection(Point direction);
+
+    /**
+     * @brief setStep
+     * Sets the step along the ray.
+     * @param step
+     * Step value.
+     */
+    __device__
+    void setStep(float step);
+
+    /**
+     * @brief getCurrentPos
+     * @return
+     * The current position of the ray.
+     */
+    __device__
+    Point getCurrentPos() const;
+
+    /**
+     * @brief getDirection
+     * @return
+     * The current direction of the ray.
+     */
+    __device__
+    Point getDirection() const;
+
+    /**
+     * @brief getStep
+     * @return
+     * The current step along the ray.
+     */
+    __device__
+    float getStep() const;
+
+    /**
+     * @brief move
+     * The point moves in the specified direction with the given step.
+     * The function relies on member attributes that getYou should set first.
+     */
+    __device__
+    void move();
 
 private:
+
+    /**
+     * @brief _currentPos
+     * The current position of the ray.
+     */
     Point _currentPos;
+
+    /**
+     * @brief _direction
+     * The current direction unit vector of the ray.
+     */
     Point _direction;
+
+    /**
+     * @brief _step
+     * The step along the ray.
+     */
     float _step;
 };
 #endif
