7084c6e5264775383eefaa11462e482bd1bf124b
Eman Mostafa <you@example.com>
2020-02-18 02:12:09 +0200

    Network-Cmake

---
+++ build/CMakeFiles/NetworkDemo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eman/3D-Random-Walk-CUDA/Network/Sources/socket.cpp" "/home/eman/3D-Random-Walk-CUDA/build/CMakeFiles/NetworkDemo.dir/Network/Sources/socket.cpp.o"
+  "/home/eman/3D-Random-Walk-CUDA/build/NetworkDemo_autogen/mocs_compilation.cpp" "/home/eman/3D-Random-Walk-CUDA/build/CMakeFiles/NetworkDemo.dir/NetworkDemo_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "NetworkDemo_autogen/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/NetworkDemo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NetworkDemo.
+# This may be replaced when dependencies are built.
