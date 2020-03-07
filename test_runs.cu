fe205d8374747eae450aaebaa15aef02c65232db
Mustafa Asaad <mostafa0as3d@gmail.com>
2020-03-07 15:51:46 +0200

    Metrics printing!

--- test_runs.cu
+++ test_runs.cu
@@ -1,13 +1,13 @@
 #include "code/headers/randomwalk.h"
 
-#define NUMBER_OF_PHOTONS 1000
+//#define NUMBER_OF_PHOTONS 1000
 #define THREADS_PER_BLOCK 1024
 #define DETECTOR_RADIUS 10.f
 #define DETECTOR_POSITION Point(0.f, 0.f, 50.f)
 #define DETECTOR_LOOKAT Vector(0.f, 0.f, -1.f)
 #define TISSUE_RADIUS 100.f
 #define TISSUE_ABSORBTION_COEFFICIENT 1.f
-#define TISSUE_SCATTERING_COEFFICIENT 100.f
+//#define TISSUE_SCATTERING_COEFFICIENT 100.f
 #define TISSUE_CENTER_1 Point(0.f, 0.f, 50.f)
 #define TISSUE_CENTER_2 Point(0.f, 0.f, -50.f)
 
@@ -23,8 +23,12 @@ __global__ void finalState(unsigned int seed, curandState_t *states, Photon *_gp
 }
 
 int main( int argc, char *argv[] ){
-    int NUMBER_OF_TEST_RUNS = std::atoi(argv[1]);
+    int NUMBER_OF_PHOTONS = std::atoi(argv[1]);
+    unsigned int NUMBER_OF_TEST_RUNS = 10000;
+    float TISSUE_SCATTERING_COEFFICIENT  = std::atoi(argv[2]);
     float totalTime = 0;
+    unsigned int totalLifetime = 0;
+    float detectedRatio, terminatedRatio, escapedRatio = 0.f;
         for (int i= 0; i<NUMBER_OF_TEST_RUNS; i++){
             int nBlocks = NUMBER_OF_PHOTONS / THREADS_PER_BLOCK + 1;
             RNG rng;
@@ -49,10 +53,28 @@ int main( int argc, char *argv[] ){
             totalTime += milliseconds;
             cudaEventDestroy( start );
             cudaEventDestroy( stop );
+            for (int j=0; j<NUMBER_OF_PHOTONS; j++){
+                totalLifetime += _cpuPhotons[j].getLifetime();
+                if ( _cpuPhotons[j].getState() == Photon::DETECTED){
+                    detectedRatio += 1;
+                } else if ( _cpuPhotons[j].getState() == Photon::TERMINATED){
+                    terminatedRatio += 1;
+                } else {
+                    escapedRatio += 1 ;
+                }
+            }
             free(_cpuPhotons);
             cudaFree(_gpuPhotons);
             cudaFree(states);
         }
-        std::cout << "Average = " << totalTime/NUMBER_OF_TEST_RUNS << " ms" << std::endl;
+        std::cout << "# Photons = " << NUMBER_OF_PHOTONS << std::endl;
+        std::cout << "Attenuation Coefficient = " << TISSUE_ABSORBTION_COEFFICIENT + TISSUE_SCATTERING_COEFFICIENT << std::endl;
+        std::cout << "Average Time = " << totalTime/NUMBER_OF_TEST_RUNS << " ms" << std::endl;
+        std::cout << "Average Total # of Walks = " << totalLifetime/NUMBER_OF_TEST_RUNS << " walks" << std::endl;
+        std::cout << "Average Detected/Total = " << (detectedRatio/NUMBER_OF_TEST_RUNS)/NUMBER_OF_PHOTONS << std::endl;
+        std::cout << "Average Terminated/Total = " << (terminatedRatio/NUMBER_OF_TEST_RUNS)/NUMBER_OF_PHOTONS << std::endl;
+        std::cout << "Average Escaped/Total = " << (escapedRatio/NUMBER_OF_TEST_RUNS)/NUMBER_OF_PHOTONS << std::endl;
+        std::cout << "#Threads/Block = " << THREADS_PER_BLOCK << std::endl;
+        std::cout << "# Blocks = " << NUMBER_OF_PHOTONS / THREADS_PER_BLOCK + 1 << std::endl;
         return 0;
 }
